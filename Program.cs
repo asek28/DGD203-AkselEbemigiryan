a790f1dc7a7507adf17ba27374cfc5db23202d6c
asek28 <aksel.ebe@gmail.com>
2024-11-12 11:41:09 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameController controller = new GameController();
+
+            controller.StartGame();
+        }
+    }
+}
